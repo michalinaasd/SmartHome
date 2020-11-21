867fef00f1aa04806a8b7de6ec4d5a9ee158b414
Michalina Kmieciak <michalinaasd@gmail.com>
2020-11-21 13:16:11 +0100

    apollo client

--- App.js
+++ App.js
@@ -2,15 +2,23 @@ import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import BottomMenu from './app/components/BottomMenu';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 
 const Drawer = createDrawerNavigator();
 
+const client = new ApolloClient({
+  uri: 'localhost:8000/graphql',
+  cache: new InMemoryCache(),
+});
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={BottomMenu} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Drawer.Navigator initialRouteName="Home">
+          <Drawer.Screen name="Home" component={BottomMenu} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
--- app/components/Rooms.js
+++ app/components/Rooms.js
@@ -2,8 +2,27 @@ import React from 'react';
 import {View, StyleSheet, Text} from 'react-native';
 import RoomItem from './RoomItem';
 import {rooms} from './constants';
+import {useQuery, gql} from '@apollo/client';
 
+const GET_ROOMS = gql`
+  query GET_ROOMS {
+    building(id: 1) {
+      id
+    }
+  }
+`;
 const Rooms = ({navigation}) => {
+  const {loading, data, error} = useQuery(GET_ROOMS);
+
+  if (loading) {
+    console.log('loading');
+  }
+  if (data) {
+    console.log(data);
+  }
+  if (error) {
+    console.log(error);
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Rooms</Text>
