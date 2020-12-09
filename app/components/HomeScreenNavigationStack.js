a39f0b2e2ec51e77010ea6749bdafaa8ff8d79a2
Michalina Kmieciak <michalinaasd@gmail.com>
2020-12-09 14:22:10 +0100

    Setting up the device states

--- app/components/HomeScreenNavigationStack.js
+++ app/components/HomeScreenNavigationStack.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
-import HomeScreen from './HomeScreen';
-import RoomScreen from './RoomScreen/RoomScreen';
-import CreateSceneIcon from './CreateScene/CreateSceneIcon';
-import CreateSceneName from './CreateScene/CreateSceneName';
-import CreateSceneDevices from './CreateScene/CreateSceneDevices';
-import {rooms} from './constants';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./HomeScreen";
+import RoomScreen from "./RoomScreen/RoomScreen";
+import CreateSceneIcon from "./CreateScene/CreateSceneIcon";
+import CreateSceneName from "./CreateScene/CreateSceneName";
+import CreateSceneDevices from "./CreateScene/CreateSceneDevices";
+import CreateSceneDevicesStates from "./CreateScene/CreateSceneDevicesStates";
+import { rooms } from "./constants";
 
 const Stack = createStackNavigator();
 
@@ -15,27 +16,32 @@ const HomeScreenNavigationStack = () => {
       <Stack.Screen
         name="home"
         component={HomeScreen}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
       {rooms.map((name) => (
-        <Stack.Screen key={name} name={name} options={{headerShown: false}}>
+        <Stack.Screen key={name} name={name} options={{ headerShown: false }}>
           {(props) => <RoomScreen name={name} />}
         </Stack.Screen>
       ))}
       <Stack.Screen
         name="create-scene"
         component={CreateSceneIcon}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
       <Stack.Screen
         name="create-scene-name"
         component={CreateSceneName}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
       <Stack.Screen
         name="create-scene-devices"
         component={CreateSceneDevices}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="create-scene-devices-states"
+        component={CreateSceneDevicesStates}
+        options={{ headerShown: false }}
       />
     </Stack.Navigator>
   );
